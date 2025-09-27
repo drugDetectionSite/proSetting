20d323e79dcc36a4aca75f562318eec0ba25f6a2
KGyeongSeo <loljyeo@gmail.com>
2025-09-27 18:09:10 +0900

    feat:app.py에 html파일 적용

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'dkdjflkjlkajlkj!'
+def home():
+    return render_template('homePage.html')
 
 if __name__ == '__main__':
     print("start")
