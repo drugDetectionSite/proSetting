10b55e59e8ff871b46d945723bf1ba732e16ecdd
JYuRim1026 <sky13512@naver.com>
2025-12-21 15:03:03 +0900

    Update db.py

--- api/db.py
+++ api/db.py
@@ -1,5 +1,11 @@
 # 커넥션 풀 유틸 함수 등 DB를 연결하는 파일입니다.
 # 유림님 작성할 곳
+from model import add_new_slang, show_all_slangs, extract_suspicious_parts
 
 def get_db():
-    return None
+    try:
+        conn = mysql.connector.connect(**DB_CONFIG)
+        return conn
+    except Exception as e:
+        print(f"DB 연결 실패: {e}")
+        return None
